e0b983646525d962a68583e58e9af868ab290b83
Dexter Lohnes <dexterlohnes@gmail.com>
2016-05-17 10:11:10 -0700

    +Implemented 8.5 and 8.6. Create rich join between Section and AdminUsers

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,10 @@
 class AdminUser < ActiveRecord::Base
 
+	has_and_belongs_to_many :pages
+	has_many :section_edits
+	# Reach 'through' :section_edits to get :sections
+	has_many :sections, :through => :section_edits
+
 	scope :search, lambda { |query| 
 		where (["username LIKE ?", "%#{query}%"]) }
 
